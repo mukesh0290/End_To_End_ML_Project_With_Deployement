2047f5942445f5775a435418abe87ff4bbf6f2e3
mukesh0290 <mukesh0290@gmail.com>
2024-03-26 12:12:52 +0530

    github workflow added

--- app.py
+++ app.py
@@ -42,6 +42,5 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
-
-    # http://127.0.0.1:5000/predictdata 
+    # app.run(host="0.0.0.0",port=8080)        
+    app.run(host='0.0.0.0', port=8080)
